48c9105954baeacd50097e66fbf772f3c9c8b3cd
Faiz uddin <faizu6639@gmail.com>
2026-06-26 01:30:06 +0500

    reviews update

--- src/assets/data/reviewData.ts
+++ src/assets/data/reviewData.ts
@@ -3,30 +3,36 @@ export const reviews = [
     author: "greenbasketz (63)",
     location: "Verified Purchase",
     text: "Quick delivery, great quality product, and unbeatable price. Very satisfied, thanks! The part condition was brand new and appearance was perfect. Highly recommended.",
-    meta: "High Temp Silicone Gasket Sealant Black +350°C"
+    meta: "High Temp Silicone Gasket Sealant Black +350°C Heat Resistant 21g"
   },
   {
     author: "kwiktrans (659)",
     location: "Verified Purchase",
     text: "Item as described",
-    meta: "DIESEL ENGINE 204DTD CONNECTING CONROD For JAGUAR E-PACE XE XF 2.0 D"
+    meta: "DIESEL ENGINE 204DTD CONNECTING CONROD For JAGUAR E-PACE XE XF 2.0 D TD4 AWD"
   },
   {
     author: "5400brenda (1043)",
     location: "Verified Purchase",
     text: "As described fast delivery many thanks",
-    meta: "LAND ROVER JAGUAR 204DTD SINGLE TURBO PISTON RINGS SET STD SIZE"
+    meta: "LAND ROVER JAGUAR 204DTD SINGLE TURBO 150-180HP AJ200D PISTON RINGS SET STD SIZE"
   },
   {
     author: "no1! (534)",
     location: "Verified Purchase",
     text: "Perfect timing arrived same day as sump hopefully be fitted soon and without an issue! A1 service many thanks supper pleased!",
-    meta: "Oil Sump Engine Oil Pan RTV Silicone Gasket Sealant Hi-Temp Black"
+    meta: "Oil Sump Engine Oil Pan RTV Silicone Gasket Sealant Hi-Temp Black 85g"
   },
   {
     author: "vadkrasn888 (304)",
     location: "Verified Purchase",
     text: "Excellent seller! Fast shipping, item exactly as described, and great communication throughout. Highly recommended—would definitely buy again.",
     meta: "Black RTV Silicone High Temp Gasket Maker Sealant 85g Tube"
+  },
+  {
+    author: "rocky-2311 (709)",
+    location: "Verified Purchase",
+    text: "Very good",
+    meta: "LAND ROVER JAGUAR 2.0 DIESEL ENGINE AJ200 204DTD 204DTA TIMING CHAIN KIT"
   }
 ];
